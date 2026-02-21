17c4cfe368066cc0e6700b58627abb0efd55ef0f
David Genotelle <david.genotelle@gmail.com>
2026-02-21 22:53:51 +0100

    Initial commit

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnLoad() {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 50) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnLoad);
+window.addEventListener("load", revealOnLoad);
